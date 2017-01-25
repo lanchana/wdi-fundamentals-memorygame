ffafc94d30a61e25bf7d4f8668dc75379a21f12a
lanchanashivananda <lanchanagupta@gmail.com>
2017-01-25 01:01:13 -0500

    include javascript file

---
+++ starter-code/javascript/main.js
@@ -0,0 +1,6 @@
+console.log("JS file is connected to HTML! Woo!");
+var cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour="king";
+console.log(cardOne == cardTwo);
+console.log(cardThree == cardFour);
+console.log(cardOne == cardFour);
+console.log(cardThree == cardTwo);
