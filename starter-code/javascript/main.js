8d8a533320c804d2b081e0fcded08bf51ce15da2
lanchanashivananda <lanchanagupta@gmail.com>
2017-01-25 23:26:38 -0500

    create card div element using javascript: assignment 10  Please enter the commit message for your changes. Lines starting

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,24 +1,49 @@
 console.log("JS file is connected to HTML! Woo!");
-var cardOne = "queen", cardTwo = "queen", cardThree = "king", cardFour="king";
-console.log(cardOne == cardTwo);
-console.log(cardThree == cardFour);
-console.log(cardOne == cardFour);
-console.log(cardThree == cardTwo);
-if(cardTwo === cardFour) {
-    alert("You found a match!");
-}
-else {
-    alert("Sorry, try again.");
-}
-if(cardOne === cardTwo) {
-    alert("You found a match!");
-}
-else {
-    alert("Sorry, try again.");
-}
-if(cardThree === cardFour) {
-    alert("You found a match!");
-}
-else {
-    alert("Sorry, try again.");
-}
+
+var cardOne = 'queen', cardTwo = 'queen', cardThree = 'king', cardFour='king';
+var divId = document.getElementById('game-board');
+
+var createCards = function() {
+    for(var i = 0; i < 4; i++) {
+        var innerDiv = document.createElement('div');
+        innerDiv.className = 'card';
+        divId.appendChild(innerDiv);
+    }
+};
+
+createCards();
+
+// console.log(cardOne == cardTwo);
+// console.log(cardThree == cardFour);
+// console.log(cardOne == cardFour);
+// console.log(cardThree == cardTwo);
+// if(cardTwo === cardFour) {
+//     alert("You found a match!");
+// }
+// else {
+//     alert("Sorry, try again.");
+// }
+// if(cardOne === cardTwo) {
+//     alert("You found a match!");
+// }
+// else {
+//     alert("Sorry, try again.");
+// }
+// if(cardThree === cardFour) {
+//     alert("You found a match!");
+// }
+// else {
+//     alert("Sorry, try again.");
+// }
+
+
+
+
+
+
+
+
+
+
+
+
