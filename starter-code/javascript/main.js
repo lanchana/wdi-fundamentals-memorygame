9647f1c6eca10b81140e7ef8a077d2545c2f6c63
lanchanashivananda <lanchanagupta@gmail.com>
2017-01-28 00:22:14 -0500

    included functions to check no. of cards in play and to check two cards are a match

--- starter-code/javascript/main.js
+++ starter-code/javascript/main.js
@@ -1,46 +1,76 @@
-console.log("JS file is connected to HTML! Woo!");
+// console.log("JS file is connected to HTML! Woo!");
 
-var cardOne = 'queen', cardTwo = 'queen', cardThree = 'king', cardFour='king';
-var divId = document.getElementById('game-board');
+// // var cardOne = 'queen', cardTwo = 'queen', cardThree = 'king', cardFour='king';
+// var cards = ['queen', 'queen', 'king', 'king'];
+// var cardsInPlay =[];
+
+var cards =['queen', 'queen', 'king', 'king'];
+var cardsInPlay =[];
+var gameBoard = document.getElementById('game-board');
+var cardElement = document.getElementsByClassName('card');
+var win = 0;
+var lose = 0;
+// var reset =document.getElementById('reset');
 
 var createCards = function() {
-    for(var i = 0; i < 4; i++) {
-        var innerDiv = document.createElement('div');
-        innerDiv.className = 'card';
-        divId.appendChild(innerDiv);
+    for(var i = 0; i < cards.length; i++) {
+        var cardDiv = document.createElement('div');
+        cardDiv.className = 'card';
+        gameBoard.appendChild(cardDiv);
+    }
+    createBoard();
+};
+
+var createBoard = function() {
+    for (var i = 0; i < cards.length; i++) {
+        cardElement[i].setAttribute('data-card', cards[i]);
+        cardElement[i].addEventListener('click', isTwoCards);
+    }
+}
+
+var isTwoCards = function() {
+    cardsInPlay.push(this.getAttribute('data-card'));
+console.log("asfds");
+  if(this.getAttribute('data-card') === 'queen') {
+        this.innerHTML = '<img src="images/diamonds-884199_1280.png" alt="Queen of Spades"/>';
+    }
+    else{
+        this.innerHTML = '<img src="images/spades-884197_1280.png" alt="King of Spades"/>';
+    }
+if (cardsInPlay.length === 2) {
+    isMatch(cardsInPlay);
+    cardsInPlay = [];
+}
+
+}
+
+var isMatch = function() {
+    if(cardsInPlay[0] === cardsInPlay[1]){
+        alert("You found a match!");
+        win++;
+        // cardElement.innerHTML = '';
+    }
+    else
+    {
+        alert("Sorry, try again.");
+        lose++;
+    }
+    // for(var i = 0; i < cardElement.length; i++){
+//         cardElement[i].innerHTML = " ";
+//     }
+}
+
+// reset.addEventListener('click', clear);
+document.getElementById("reset").onclick = function() {
+   for(var i = 0; i < cardElement.length; i++){
+        cardElement[i].innerHTML = " ";
     }
 };
 
+document.getElementById("score").onclick = function() {
+   alert("your score\n" + "No. of Wins: " + win + "\nNo of Loses: " + lose);
+};
 createCards();
-
-// console.log(cardOne == cardTwo);
-// console.log(cardThree == cardFour);
-// console.log(cardOne == cardFour);
-// console.log(cardThree == cardTwo);
-// if(cardTwo === cardFour) {
-//     alert("You found a match!");
-// }
-// else {
-//     alert("Sorry, try again.");
-// }
-// if(cardOne === cardTwo) {
-//     alert("You found a match!");
-// }
-// else {
-//     alert("Sorry, try again.");
-// }
-// if(cardThree === cardFour) {
-//     alert("You found a match!");
-// }
-// else {
-//     alert("Sorry, try again.");
-// }
-
-
-
-
-
-
 
 
 
